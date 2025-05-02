9ca73eae706ee50434e9b856a619ffbc68487f9d
Anjudevi234 <ponnucharuvilayil2004@gmail.com>
2025-05-02 12:18:24 +0530

    Add files via upload

---
+++ Authentication.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Authentication 
+{
+    public static void main(String args[])
+    {
+        String username="Ashish";
+        String password="@Ashish123";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the username");
+        String u1=sc.nextLine();
+        System.out.println("Enter the password");
+        String u2=sc.nextLine();
+        try
+        {
+           if((u1.equals(username))&&(u2.equals(password)))
+           {
+            System.out.println("Acess Granted");
+           }
+        else
+        {
+            throw new CredentialExpection("invalid Credentials");
+        }
+    }
+    catch(CredentialExpection e)
+    {
+        System.out.println(e.getMessage());
+    }
+    }
+}
+
